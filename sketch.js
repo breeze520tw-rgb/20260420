d1c6a42f3eed9d8b55fba13bb45671ac15aae26c
breeze520tw-rgb <mynameisbreeze520.tw@gmail.com>
2026-04-20 10:24:41 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let capture;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // 取得攝影機影像
+  capture = createCapture(VIDEO);
+  // 隱藏預設的 DOM 影片元件，只在畫布上繪製
+  capture.hide();
+}
+
+function draw() {
+  background('#e7c6ff');
+
+  // 計算影像寬高 (畫布寬高的 60%)
+  let vWidth = width * 0.6;
+  let vHeight = height * 0.6;
+
+  // 計算置中位置
+  let x = (width - vWidth) / 2;
+  let y = (height - vHeight) / 2;
+
+  // 繪製影像到畫布上
+  image(capture, x, y, vWidth, vHeight);
+}
+
+function windowResized() {
+  // 當視窗大小改變時，重新調整畫布尺寸
+  resizeCanvas(windowWidth, windowHeight);
+}
